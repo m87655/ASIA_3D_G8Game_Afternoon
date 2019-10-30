82b4f970db8dfd60fc4d15ee8221494dfc8deed5
unknown <suchild7@gmail.com>
2019-10-30 15:57:46 +0800

    完成動畫控制並添加腳本

---
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;  // 引用 Unity API
+
+// 類別 類別名稱
+public class Chicken : MonoBehaviour
+{
+    // 宣告變數 (定義欄位 Field)
+    // 欄位類型 欄位名稱 (指定 值) 結束
+    int speed = 10;             // 整數 1, 9999, -100
+    float turn = 20.5f;         // 浮點數
+    bool mission;               // 布林值 true false
+    string _name = "G8雞";      // 字串 ""
+}
