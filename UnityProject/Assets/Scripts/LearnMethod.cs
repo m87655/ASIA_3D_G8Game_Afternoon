af8adb4542ca72ac90c43aec1f46c4d262a78230
KID <50666207+KID421@users.noreply.github.com>
2019-11-20 14:48:03 +0800

    方法參數定義與呼叫用法

--- UnityProject/Assets/Scripts/LearnMethod.cs
+++ UnityProject/Assets/Scripts/LearnMethod.cs
@@ -4,13 +4,16 @@ public class LearnMethod : MonoBehaviour
 {
     // 定義方法
     // 語法：
-    // 修飾詞 傳回類型 方法名稱 () { 陳述式或演算法 }
+    // 修飾詞 傳回類型 方法名稱 (參數) { 陳述式或演算法 }
+    // 參數類型 參數名稱
     // void 無傳回
     // 需要被呼叫
-    private void Drive()
+    private void Drive(int speed)
     {
         // 輸出訊息(字串);
-        print("開車，時速：200");
+        print("音效~");
+        // 可使用 + 號串接字串與其他類型
+        print("開車，時速：" + speed);
     }
 
     // 事件：在指定的時間會以指定次數執行的方法
@@ -19,7 +22,9 @@ public class LearnMethod : MonoBehaviour
     {
         print("哈囉，沃德~");
 
-        Drive();    // 呼叫自訂方法
-        Drive();
+        // 呼叫自訂方法，需給予對應的引數
+        Drive(200);    
+        Drive(300);
+        Drive(400);
     }
 }
