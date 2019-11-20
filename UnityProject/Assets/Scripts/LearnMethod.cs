a5238ef942d3d2dd80e5190fab9465e2e154c3a8
KID <50666207+KID421@users.noreply.github.com>
2019-11-20 14:18:15 +0800

    認識如何定義方法

---
+++ UnityProject/Assets/Scripts/LearnMethod.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LearnMethod : MonoBehaviour
+{
+    // 定義方法
+    // 語法：
+    // 修飾詞 傳回類型 方法名稱 () { 陳述式或演算法 }
+    // void 無傳回
+    private void Drive()
+    {
+        // 輸出訊息(字串);
+        print("開車，時速：200");
+    }
+}
