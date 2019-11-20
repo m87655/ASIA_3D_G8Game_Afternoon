b34c14a64e10e923c92f8def32d940f5e4b67a24
KID <50666207+KID421@users.noreply.github.com>
2019-11-20 13:57:30 +0800

    認識欄位屬性：標題、提示與範圍

--- UnityProject/Assets/Scripts/Chicken.cs
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -4,9 +4,15 @@
 public class Chicken : MonoBehaviour
 {
     // 宣告變數 (定義欄位 Field)
-    // 欄位類型 欄位名稱 (指定 值) 結束
-    int speed = 10;             // 整數 1, 9999, -100
-    float turn = 20.5f;         // 浮點數
-    bool mission;               // 布林值 true false
-    string _name = "G8雞";      // 字串 ""
+    // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束
+    // 私人 - 隱藏 private (預設)
+    // 公開 - 顯示 public 
+    [Header("移動速度")][Range(1, 100)]
+    public int speed = 10;             // 整數 1, 9999, -100
+    [Header("旋轉速度"), Tooltip("G8雞的旋轉速度"), Range(1.5f, 200f)]
+    public float turn = 20.5f;         // 浮點數
+    [Header("是否完成任務")]
+    public bool mission;               // 布林值 true false
+    [Header("玩家名稱")]
+    public string _name = "G8雞";      // 字串 ""
 }
