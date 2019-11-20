fd4f46c8ea6bfba0e031aaeacc7a7afa53afe7d0
KID <50666207+KID421@users.noreply.github.com>
2019-11-20 14:36:31 +0800

    認識初始事件與呼叫方法

--- UnityProject/Assets/Scripts/LearnMethod.cs
+++ UnityProject/Assets/Scripts/LearnMethod.cs
@@ -6,9 +6,20 @@ public class LearnMethod : MonoBehaviour
     // 語法：
     // 修飾詞 傳回類型 方法名稱 () { 陳述式或演算法 }
     // void 無傳回
+    // 需要被呼叫
     private void Drive()
     {
         // 輸出訊息(字串);
         print("開車，時速：200");
+    }
+
+    // 事件：在指定的時間會以指定次數執行的方法
+    // 初始事件：遊戲播放後執行一次
+    private void Start()
+    {
+        print("哈囉，沃德~");
+
+        Drive();    // 呼叫自訂方法
+        Drive();
     }
 }
