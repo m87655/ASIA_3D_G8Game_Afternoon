667a1acf0c1e4c4648f1884e11c04a8bf2551876
KID <50666207+KID421@users.noreply.github.com>
2019-11-20 15:17:34 +0800

    多重參數與選填式參數

--- UnityProject/Assets/Scripts/LearnMethod.cs
+++ UnityProject/Assets/Scripts/LearnMethod.cs
@@ -8,12 +8,25 @@ public class LearnMethod : MonoBehaviour
     // 參數類型 參數名稱
     // void 無傳回
     // 需要被呼叫
+    /// <summary>
+    /// 開車方法
+    /// </summary>
+    /// <param name="speed">開車的速度</param>
     private void Drive(int speed)
     {
         // 輸出訊息(字串);
         print("音效~");
         // 可使用 + 號串接字串與其他類型
         print("開車，時速：" + speed);
+    }
+
+    // 有預設值的參數 選填式參數，呼叫時可不填寫
+    // 選填式參數只能寫在最右邊
+    public void Shoot(int count, float speed, string prop = "無")
+    {
+        print("弓箭數量：" + count);
+        print("弓箭速度：" + speed);
+        print("弓箭屬性：" + prop);
     }
 
     // 事件：在指定的時間會以指定次數執行的方法
@@ -26,5 +39,9 @@ public class LearnMethod : MonoBehaviour
         Drive(200);    
         Drive(300);
         Drive(400);
+        Drive(500);
+
+        Shoot(1, 1.5f);
+        Shoot(10, 10.5f, "火屬性");
     }
 }
