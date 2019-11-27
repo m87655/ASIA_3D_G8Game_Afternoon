3618b72335bbff460940f3c28f4ab74f3f30ed92
KID <50666207+KID421@users.noreply.github.com>
2019-11-27 13:41:05 +0800

    練習：定義 G8 雞方法

--- UnityProject/Assets/Scripts/Chicken.cs
+++ UnityProject/Assets/Scripts/Chicken.cs
@@ -3,6 +3,7 @@
 // 類別 類別名稱
 public class Chicken : MonoBehaviour
 {
+    #region 欄位區域
     // 宣告變數 (定義欄位 Field)
     // 修飾詞 欄位類型 欄位名稱 (指定 值) 結束
     // 私人 - 隱藏 private (預設)
@@ -15,6 +16,47 @@ public class Chicken : MonoBehaviour
     public bool mission;               // 布林值 true false
     [Header("玩家名稱")]
     public string _name = "G8雞";      // 字串 ""
+    #endregion
 
+    #region 方法區域
+    /// <summary>
+    /// 跑步
+    /// </summary>
+    private void Run()
+    {
 
+    }
+
+    /// <summary>
+    /// 旋轉
+    /// </summary>
+    private void Turn()
+    {
+
+    }
+
+    /// <summary>
+    /// 亂叫
+    /// </summary>
+    private void Bark()
+    {
+
+    }
+
+    /// <summary>
+    /// 撿東西
+    /// </summary>
+    private void Catch()
+    {
+
+    }
+
+    /// <summary>
+    /// 檢視任務
+    /// </summary>
+    private void Task()
+    {
+
+    }
+    #endregion
 }
