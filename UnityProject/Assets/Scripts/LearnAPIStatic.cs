d495cbadbecf0dbdf4b7a15d0a825f5428688084
KID <50666207+KID421@users.noreply.github.com>
2019-11-27 15:17:10 +0800

    靜態成員：除錯與時間 - 除錯輸出與遊戲時間

--- UnityProject/Assets/Scripts/LearnAPIStatic.cs
+++ UnityProject/Assets/Scripts/LearnAPIStatic.cs
@@ -3,13 +3,29 @@
 public class LearnAPIStatic : MonoBehaviour
 {
     // 靜態
-    // 隨機、數學、輸入、時間
+    // 隨機、數學、輸入、時間、除錯
 
     private void Start()
     {
         // 使用靜態屬性
         // 類別名稱.靜態屬性
-        print(Random.value);
+        // 取得靜態屬性
+        print(Random.value);    // 0 - 1
         print(Mathf.PI);
+
+        // 類別名稱.靜態方法(對應引數)
+        print(Mathf.Abs(-99));
+        print(Random.Range(50, 150));
+
+        Debug.Log("測試~");
+        Debug.LogWarning("警告!");
+        Debug.LogError("危險!");
+    }
+
+    // 更新事件：一秒執行約 60 次
+    // 監聽玩家輸入事件、時間
+    private void Update()
+    {
+        print(Time.time);
     }
 }
---
+++ UnityProject/Assets/Scripts/TEST.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class TEST : MonoBehaviour
+{
+    private void Update()
+    {
+        Vector3 posMouse = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        posMouse.z = 10;
+        Vector3 pos = Camera.main.ScreenToWorldPoint(posMouse);
+        GetComponent<Rigidbody>().MovePosition(pos);
+    }
+}
