d811973848df0c8c485c498aeaa1e1790400bddf
KID <50666207+KID421@users.noreply.github.com>
2019-12-04 13:33:23 +0800

    數學運算子：加減乘除餘

---
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+{
+    public int A = 10, B = 3;
+
+    private void Start()
+    {
+        #region 數學運算子
+        print(A + B);
+        print(A - B);
+        print(A * B);
+        print(A / B);
+        print(A % B);
+        #endregion
+    }
+}
