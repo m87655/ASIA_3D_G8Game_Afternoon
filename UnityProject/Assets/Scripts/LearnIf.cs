8b01b0f4bd1d7a23cd79543c883ebd591bcc3d58
KID <50666207+KID421@users.noreply.github.com>
2019-12-04 15:04:51 +0800

    認識判斷式 if

---
+++ UnityProject/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    private void Start()
+    {
+        // 語法：if (布林值) { 陳述式或演算法 }
+        // () 內的布林值為 true 執行 {}
+        if (true)
+        {
+            print("測試!");
+        }
+    }
+}
