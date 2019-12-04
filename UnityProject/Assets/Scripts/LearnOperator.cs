3e7ee7c344683ed3a06fafe1d45fb22afa2a3634
KID <50666207+KID421@users.noreply.github.com>
2019-12-04 13:45:18 +0800

    數學運算子：指派、遞增與遞減

--- UnityProject/Assets/Scripts/LearnOperator.cs
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -3,6 +3,8 @@
 public class LearnOperator : MonoBehaviour
 {
     public int A = 10, B = 3;
+    public int C = 1;
+    public int D = 1, E = 1;
 
     private void Start()
     {
@@ -12,6 +14,15 @@ public class LearnOperator : MonoBehaviour
         print(A * B);
         print(A / B);
         print(A % B);
+
+        // 指派右邊先運算再給左邊
+        C = 99 + 1;
+        print(C);
+
+        C = C + 1; // 原始寫法
+        // 遞增、遞減 ++ --
+        print(D++); // 後置遞增：先輸出再運算 - 看到 1 電腦內 2
+        print(++E); // 前置遞增：先運算再輸出 - 看到 2 電腦內 2
         #endregion
     }
 }
