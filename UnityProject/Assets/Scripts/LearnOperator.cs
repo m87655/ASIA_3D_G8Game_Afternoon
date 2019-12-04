773bc4fce8e0f57bc222931c2a0fcbcc0e1c0917
KID <50666207+KID421@users.noreply.github.com>
2019-12-04 14:00:36 +0800

    數學運算子：加法指派

--- UnityProject/Assets/Scripts/LearnOperator.cs
+++ UnityProject/Assets/Scripts/LearnOperator.cs
@@ -5,6 +5,7 @@ public class LearnOperator : MonoBehaviour
     public int A = 10, B = 3;
     public int C = 1;
     public int D = 1, E = 1;
+    public int F = 10;
 
     private void Start()
     {
@@ -19,10 +20,15 @@ public class LearnOperator : MonoBehaviour
         C = 99 + 1;
         print(C);
 
-        C = C + 1; // 原始寫法
+        C = C + 1; // 原始寫法 C++
         // 遞增、遞減 ++ --
         print(D++); // 後置遞增：先輸出再運算 - 看到 1 電腦內 2
         print(++E); // 前置遞增：先運算再輸出 - 看到 2 電腦內 2
+
+        // 加法指派 - * / %
+        // F = F + 100;
+        F += 100;
+        print(F);
         #endregion
     }
 }
