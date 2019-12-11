1fbc2b597eb1eca7ed9075aaad67537375cd17df
KID <50666207+KID421@users.noreply.github.com>
2019-12-11 13:42:41 +0800

    認識如何取得非靜態屬性：變形類別 Transform 的座標 position

---
+++ UnityProject/Assets/Scripts/LearnAPI.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    // 非靜態
+    // 變形、剛體、動畫、音效、燈光、攝影機
+    // 先定義欄為存放並設定代號
+    // 修飾詞 類型 名稱(代號)
+    public Transform test1;
+
+    private void Start()
+    {
+        // 取得非靜態屬性
+        // 代號.屬性
+        print(test1.position);
+    }
+}
